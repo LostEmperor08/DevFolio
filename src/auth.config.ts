30c4c361e6ad7bd1ddbb82934ea49986691cc2f9
Samarth Patil <samarthrpatil008@gmail.com>
2026-07-25 19:00:26 +0530

    fix: provide fallback secret and enable trustHost in authConfig to resolve NextAuth Vercel error

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,8 +1,14 @@
 import NextAuth from "next-auth";
 
 export const authConfig = {
+  secret:
+    process.env.AUTH_SECRET ||
+    process.env.NEXTAUTH_SECRET ||
+    "f9b4c39c8172db7b5c87e4118f6735db9d38c642b3a985223c6f491d9b1393f9",
+  trustHost: true,
   pages: {
     signIn: "/admin/login",
+    error: "/admin/login",
   },
   session: {
     strategy: "jwt" as const,
